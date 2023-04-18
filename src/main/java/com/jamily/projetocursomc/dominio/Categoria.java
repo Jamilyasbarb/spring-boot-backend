7af01ba4aa7e9f957afc1549063f2d0e8bd85e25
Jamilyasbarb <mily.yasbarb@gmail.com>
2023-04-18 10:11:55 -0300

    add banco h2 adicionado banco h2 e a criacao automatica de base de dados

--- src/main/java/com/jamily/projetocursomc/dominio/Categoria.java
+++ src/main/java/com/jamily/projetocursomc/dominio/Categoria.java
@@ -1,9 +1,19 @@
 package com.jamily.projetocursomc.dominio;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Categoria {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Categoria implements Serializable{
+	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
