cbf81ef0a53b062df4beca908a248d6a801aae98
Jamilyasbarb <mily.yasbarb@gmail.com>
2023-04-18 20:33:35 -0300

    tratamento de erro not found

--- src/main/java/com/jamily/projetocursomc/service/CategoriaService.java
+++ src/main/java/com/jamily/projetocursomc/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.jamily.projetocursomc.dominio.Categoria;
 import com.jamily.projetocursomc.repositories.CategoriaRepository;
+import com.jamily.projetocursomc.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,7 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria>obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
