f7bb4413a5539507e74d8ad2699870fce29c8015
Jamilyasbarb <mily.yasbarb@gmail.com>
2023-04-18 08:56:32 -0300

    testando primeira classe de dominio -- Categoria

--- src/main/java/com/jamily/projetocursomc/resources/CategoriaResource.java
+++ src/main/java/com/jamily/projetocursomc/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.jamily.projetocursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jamily.projetocursomc.dominio.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Está Funcionando!";
+	public List<Categoria> listar() {
+
+		Categoria cat1 = new Categoria(1, "Sala");
+		Categoria cat2 = new Categoria(2, "Escritório");
+
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(cat1);
+		categorias.add(cat2);
+		return categorias;
 	}
 }
