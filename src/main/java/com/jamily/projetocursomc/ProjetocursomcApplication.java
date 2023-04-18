b26c2b44e5cb2f216f19e4e86b5b82f367487c54
Jamilyasbarb <mily.yasbarb@gmail.com>
2023-04-18 14:31:58 -0300

    criando operacao de instanciacao

--- src/main/java/com/jamily/projetocursomc/ProjetocursomcApplication.java
+++ src/main/java/com/jamily/projetocursomc/ProjetocursomcApplication.java
@@ -1,13 +1,32 @@
 package com.jamily.projetocursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetocursomcApplication {
+import com.jamily.projetocursomc.dominio.Categoria;
+import com.jamily.projetocursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjetocursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetocursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+	}
+	
 }
