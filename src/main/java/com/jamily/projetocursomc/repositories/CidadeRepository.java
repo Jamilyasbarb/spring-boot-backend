850efb7e65d7cf5fd372ecc0f7d62f389e04ba2a
Jamilyasbarb <mily.yasbarb@gmail.com>
2023-04-20 09:05:34 -0300

    adicionado cidade e estado

--- src/main/java/com/jamily/projetocursomc/repositories/CidadeRepository.java
+++ src/main/java/com/jamily/projetocursomc/repositories/CidadeRepository.java
@@ -3,9 +3,9 @@ package com.jamily.projetocursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.jamily.projetocursomc.dominio.Produto;
+import com.jamily.projetocursomc.dominio.Cidade;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
 
 }
--- src/main/java/com/jamily/projetocursomc/repositories/EstadoRepository.java
+++ src/main/java/com/jamily/projetocursomc/repositories/EstadoRepository.java
@@ -3,9 +3,9 @@ package com.jamily.projetocursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.jamily.projetocursomc.dominio.Produto;
+import com.jamily.projetocursomc.dominio.Estado;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
 
 }
