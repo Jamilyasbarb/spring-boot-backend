66994ca0b2628521e1247de396ccea23344052f4
Jamilyasbarb <mily.yasbarb@gmail.com>
2023-05-03 10:26:52 -0300

    configuracao inicial do spring security

--- src/main/java/com/jamily/projetocursomc/config/SecurityConfig.java
+++ src/main/java/com/jamily/projetocursomc/config/SecurityConfig.java
@@ -1,8 +1,19 @@
 package com.jamily.projetocursomc.config;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 
@@ -10,9 +21,39 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private Environment env;
+	
 	private static final String[]PUBLIC_MATCHERS = {
 			"/h2-console/**",
+	};
+	
+	private static final String[]PUBLIC_MATCHERS_GET= {
 			"/produtos/**",
 			"/categorias/**",
 	};
+	
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception{
+		
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			httpSecurity.headers().frameOptions().disable();
+		}
+		httpSecurity.cors().and().csrf().disable(); //chama CorsConfigurationSource e desativa o csrf
+		httpSecurity.authorizeRequests()
+		.antMatchers(PUBLIC_MATCHERS).permitAll() //endpoints sem autentificacao
+		.antMatchers(HttpMethod.GET,PUBLIC_MATCHERS_GET).permitAll() //endpoints sem autentificacao, apenas get
+		.antMatchers(PUBLIC_MATCHERS).permitAll() //endpoints sem autentificacao
+		.anyRequest().authenticated(); //endpoints com autentificacao
+		httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+		
+	}
 }
