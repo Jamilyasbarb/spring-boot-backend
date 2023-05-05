1d91bb05e5fe98f0440430761278c5fa30cbca1c
Jamilyasbarb <mily.yasbarb@gmail.com>
2023-05-05 09:23:25 -0300

    salvando primeiro arquivo no s3

--- src/main/java/com/jamily/projetocursomc/ProjetocursomcApplication.java
+++ src/main/java/com/jamily/projetocursomc/ProjetocursomcApplication.java
@@ -1,12 +1,17 @@
 package com.jamily.projetocursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.jamily.projetocursomc.service.S3Service;
+
 @SpringBootApplication
 public class ProjetocursomcApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetocursomcApplication.class, args);
@@ -15,7 +20,7 @@ public class ProjetocursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		
+		s3Service.uploadFile("D:\\images\\fotoup.png");
 	}
 
 }
